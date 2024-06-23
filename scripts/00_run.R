8216ab8cd7fa02ca18a706475148cadcbd3da2fc
hchulkim <kent0759@naver.com>
2024-06-23 14:42:16 +0900

    add 03 script into 00_run.R file.

--- scripts/00_run.R
+++ scripts/00_run.R
@@ -9,6 +9,7 @@ library(here)
 # Control which scripts run
 concordance_01_hs_cpc_ksic <- 1
 concordance_02_comtrade_hs_ksic <- 1
+concordance_03_est_region_match <- 1
 
 # RUN SCRIPTS ---------------------------------------------------------------
 
@@ -18,4 +19,5 @@ if (concordance_01_hs_cpc_ksic) source(here("scripts", "concordance_01_hs_cpc_ks
 # Use concordance table to transition hs code comtrade data to ksic industry code data.
 if (concordance_02_comtrade_hs_ksic) source(here("scripts", "concordance_02_comtrade_hs_ksic.R"), encoding = "UTF-8")
 
-
+# Use concordance table to match region data in est data.
+if (concordance_03_est_region_match) source(here("scripts", "concordance_03_est_region_match.R"), encoding = "UTF-8")
