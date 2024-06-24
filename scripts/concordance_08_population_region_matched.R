28bea3f90606bacff27757d39eb22f2fe6c47eb9
hchulkim <kent0759@naver.com>
2024-06-24 18:56:23 +0900

    initiate 08 script for making net pop growth in district, cz level.

---
+++ scripts/concordance_08_population_region_matched.R
@@ -0,0 +1,98 @@
+
+
+##############################
+# creator: Hyoungchul Kim
+# revised date: 06/24/2024
+# description: This code matches and merges region in population data. 
+##############################
+
+
+
+if (!require(pacman)) install.packages("pacman")
+pacman::p_load(fs, readxl, here, tidyverse)
+
+
+# Get the list of file paths
+population_files <- dir_ls(path = here("data", "population"), glob = "*.xlsx")
+
+# Create a named list by reading each file into a data frame and naming each element with the filename (without the path and extension)
+population <- population_files %>%
+  set_names(nm = basename(tools::file_path_sans_ext(.))) %>%
+  map(read_excel, skip = 2, na = "-", col_names = c("iso", "pop")) %>% 
+  list_rbind(names_to = "year")
+
+
+population <- population %>% 
+  mutate(year = as.numeric(str_replace(year, "pop", "")), iso=str_replace_all(iso, " ", "")) %>% 
+  select(iso, year, pop)
+
+population <- population %>% 
+  mutate(iso = str_extract(iso, "\\d{5}"))
+
+population <- population %>% 
+  filter(!is.na(iso))
+
+
+
+# read in the concordance for region stat----
+region_stat <- readxl::read_excel(here("data", "concordance", "region", "region_stat.xlsx"))
+
+region_stat <- region_stat %>% mutate(across(everything(), as.character))
+
+region_kosis <- readxl::read_excel(here("data", "concordance", "region", "region_kosis.xlsx"))
+
+region_kosis <- region_kosis %>% mutate(across(everything(), as.character))
+
+region_kosis <- region_kosis %>% mutate(city_stat=str_sub(city_stat, 1, 5))
+
+
+## important: due to change in code of certain province, I manually change such case.
+population <- population %>% 
+  mutate(iso = ifelse(str_sub(iso, 1, 2) == "51", paste0("42", str_sub(iso, 3, 5)), iso))
+
+population <- population %>% 
+  mutate(iso = ifelse(str_sub(iso, 1, 2) == "52", paste0("45", str_sub(iso, 3, 5)), iso))
+
+# now use region_stat to crosswalk change in regions (mostly to 2001 regions) -------
+population <- population %>% 
+  mutate(iso = ifelse(iso == "44825", "70000", iso))
+
+population <- population %>% 
+  mutate(iso = ifelse(iso == "43745", "80000", iso))
+
+population <- population %>% 
+  mutate(iso = ifelse(   as.numeric(str_sub(iso, 1, 2))>=40, paste0(str_sub(iso, 1, 4), "0"), iso))
+
+population <- population %>% left_join(region_kosis, by=c("iso"="city_kosis"))
+
+population <- population %>% select(-iso) %>% rename(iso = city_stat)
+
+population <- population %>% select(-city_label)
+
+# filter out na
+population <- population %>% filter(!is.na(iso))
+
+
+
+# now use stat region case
+
+ population <- population %>% left_join(region_stat, by=c("iso"="city_stat_raw"))
+
+ population <- population %>% 
+   mutate(iso = ifelse(!is.na(city_stat), city_stat, iso))
+ population <- population %>% select(-city_stat)
+
+ 
+ 
+ population <- population %>% 
+   group_by(year, iso) %>% 
+   summarise(pop = sum(pop, na.rm = T)) %>% 
+   ungroup()
+
+# also get the net pop growth
+
+# sigungu version
+
+ 
+# cz level
+
