d4a25f41286b06f0efe6effe312c5285f798095c
hchulkim <kent0759@naver.com>
2024-06-24 13:06:06 +0900

    finalize 04 script that crosswalks establishment data from ksic8, 9 to ksic10 data.

--- scripts/concordance_04_est_ksic89_ksic10.R
+++ scripts/concordance_04_est_ksic89_ksic10.R
@@ -23,495 +23,145 @@ ksic8_ksic9  <- read_rds(here("data", "concordance", "hs_cpc_ksic","ksic8_ksic9.
 ksic9_ksic10  <- read_rds(here("data", "concordance", "hs_cpc_ksic", "ksic9_ksic10.rds"))
 
 # paste industry code into 5 digit
-est1994 <- est1994 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5))
-est1996 <- est1996 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5))
-est1999 <- est1999 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5))
-est2000 <- est2000 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5))
+est1994 <- est1994 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5), year = 1994)
+est1996 <- est1996 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5), year = 1996)
+est1999 <- est1999 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5), year = 1999)
+est2000 <- est2000 %>% mutate(ksic8=paste0(ind2, ind3, ind4, ind5), year = 2000)
   
 
 
-est2001 <- est2001 %>% mutate(ksic8=paste0(`산업분류(중)`, `산업분류(소)`, `산업분류(세)`, `산업분류(세세)`), emp_female=`8-⑥종사자_합계_여(①+…+⑤)`, emp_male=`8-⑥종사자_합계_남(①+…+⑤)`, emp_all=`8-⑥종사자_합계_계(①+…+⑤)`) %>% select(iso, ksic8, emp_female, emp_male, emp_all)
+est2001 <- est2001 %>% mutate(ksic8=paste0(`산업분류(중)`, `산업분류(소)`, `산업분류(세)`, `산업분류(세세)`), emp_female=`8-⑥종사자_합계_여(①+…+⑤)`, emp_male=`8-⑥종사자_합계_남(①+…+⑤)`, emp_all=`8-⑥종사자_합계_계(①+…+⑤)`) %>% select(iso, ksic8, emp_female, emp_male, emp_all) %>% 
+  mutate(year = 2001)
 
 
-est2010 <- est2010 %>% mutate(ksic9=paste0(`산업분류_중`, `산업분류_소`, `산업분류_세`, `산업분류_세세`), emp_female=`종사자수_합계_여`, emp_male=`종사자수_합계_남`, emp_all=`종사자수_합계_계`) %>% select(iso, ksic9, emp_female, emp_male, emp_all)
+est2010 <- est2010 %>% mutate(ksic9=paste0(`산업분류_중`, `산업분류_소`, `산업분류_세`, `산업분류_세세`), emp_female=`종사자수_합계_여`, emp_male=`종사자수_합계_남`, emp_all=`종사자수_합계_계`) %>% select(iso, ksic9, emp_female, emp_male, emp_all) %>% 
+  mutate(year = 2010)
 
 
 
 est2019 <- est2019 %>% mutate(ksic10=paste0(`주사업_산업중분류코드`, `주사업_산업소분류코드`, `주사업_산업세분류코드`, `주사업_산업세세분류코드`)) %>% 
-  select(iso, ksic10, emp_female=`여자종사자수`, emp_male=`남자종사자수`, emp_all=`합계종사자수`)
+  select(iso, ksic10, emp_female=`여자종사자수`, emp_male=`남자종사자수`, emp_all=`합계종사자수`) %>% 
+  mutate(year = 2019)
 
 # save 2019 as it is already ksic10
 
 est2019 <- est2019 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
+  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup() %>% 
+  mutate(year = 2019)
+
+est2019 %>% write_rds(here("data", "temp", "est2019_region_industry_matched.rds"))
+
+
+
+# ksic 8 to 9: 1994, 1996, 1999, 2000, 2001
+
+ksic89_list <- list(est1994, est1996, est1999, est2000, est2001)
+
+# Define the function to process each dataframe
+process_ksic89 <- function(df) {
+  # Perform desired operations on the dataframe
+  
+# divide the ksic8 to ksic9 to n:1 and 1:n case and process the transition.
+# n:1 case
+  ksic8_ksic9_n1 <- ksic8_ksic9 %>% 
+   filter(weight >= 1)
+
+  df <- df %>% 
+    left_join(ksic8_ksic9_n1, by="ksic8")
+
+  # leave out the one that has NA.
+  df_1n <- df %>% 
+    filter(is.na(ksic9)) %>% 
+    select(-c("ksic9", "weight"))
+  
+  df <- df %>% 
+    filter(!is.na(ksic9)) %>% 
+    select(-weight) %>% 
+    select(-ksic8)
+  
+  # take care of the 1:n case
+  ksic8_ksic9_1n <- ksic8_ksic9 %>% 
+    filter(weight < 1)
+  
+  df_1n <- df_1n %>% 
+    left_join(ksic8_ksic9_1n, by="ksic8", relationship = "many-to-many")
+  
+  df_1n <- df_1n %>% 
+    mutate(emp_all = emp_all * weight) %>% 
+    select(-c("ksic8", "weight"))
+  
+  df <- df %>% 
+    bind_rows(df_1n) %>% 
+    group_by(iso, year, ksic9) %>% 
+    summarise(emp_all = sum(emp_all, na.rm = T)) %>% 
+    ungroup()
+
+  return(df)
+}
+
+# Apply the function to each dataframe in the list
+processed_ksic89 <- map(ksic89_list, process_ksic89) %>% 
+  list_rbind()
+
+
+
+# ksic 9 to 10: 1994, 1996, 1999, 2000, 2001, 2010
+
+ksic910_list <- list(processed_ksic89, est2010)
+
+# Define the function to process each dataframe
+process_ksic910 <- function(df) {
+  # Perform desired operations on the dataframe
+  
+# divide the ksic9 to ksic9 to n:1 and 1:n case and process the transition.
+# n:1 case
+  ksic9_ksic10_n1 <- ksic9_ksic10 %>% 
+   filter(weight >= 1)
+
+  df <- df %>% 
+    left_join(ksic9_ksic10_n1, by="ksic9")
+
+  # leave out the one that has NA.
+  df_1n <- df %>% 
+    filter(is.na(ksic10)) %>% 
+    select(-c("ksic10", "weight"))
+  
+  df <- df %>% 
+    filter(!is.na(ksic10)) %>% 
+    select(-weight) %>% 
+    select(-ksic9)
+  
+  # take care of the 1:n case
+  ksic9_ksic10_1n <- ksic9_ksic10 %>% 
+    filter(weight < 1)
+  
+  df_1n <- df_1n %>% 
+    left_join(ksic9_ksic10_1n, by="ksic9", relationship = "many-to-many")
+  
+  df_1n <- df_1n %>% 
+    mutate(emp_all = emp_all * weight) %>% 
+    select(-c("ksic9", "weight"))
+  
+  df <- df %>% 
+    bind_rows(df_1n) %>% 
+    group_by(iso, year, ksic10) %>% 
+    summarise(emp_all = sum(emp_all, na.rm = T)) %>% 
+    ungroup()
 
-est2019 %>% haven::write_dta("data/final/hs_cpc_ksic/est2019_refine.dta")
-est2019 %>% write_rds("data/final/hs_cpc_ksic/est2019_refine.rds")
+  return(df)
+}
 
+# Apply the function to each dataframe in the list
+processed_ksic910 <- map(ksic910_list, process_ksic910) %>% 
+  list_rbind()
 
-#ksic 8 to 9 : 1994 only
 
-est1994 <- est1994 %>% group_by(iso, ksic8) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
+# rbind all the ksic10 est data.
+processed_ksic10 <- processed_ksic910 %>% 
+  bind_rows(est2019 %>% 
+              group_by(iso, year, ksic10) %>% 
+              summarise(emp_all = sum(emp_all, na.rm = T)) %>% 
+              ungroup())
 
-
-ksic8_ksic9_1 <- ksic8_ksic9 %>% filter(weight==1)
-ksic8_ksic9_2 <- ksic8_ksic9 %>% filter(weight!=1)
-
-
-
-check <- ksic8_ksic9_2 %>% distinct(ksic8) %>% pull(ksic8)
-
-
-est1994_1 <- est1994 %>% filter(!(ksic8 %in% check))
-
-est1994_2 <- est1994 %>% filter(ksic8 %in% check)
-
-est1994_1 <- est1994_1 %>%
-  left_join(ksic8_ksic9_1, by="ksic8") %>% select(-ksic8)
-
-
-
-ksic8_ksic9_2 <- ksic8_ksic9_2 %>%
-  left_join(est1994_2, by="ksic8") %>% select(-ksic8)
-
-
-est1994 <- est1994_1 %>%
-  bind_rows(ksic8_ksic9_2) %>% filter(!is.na(emp_all), !is.na(ksic9))
-
-#ksic 8 to 9 : 1996 only
-
-est1996 <- est1996 %>% group_by(iso, ksic8) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-ksic8_ksic9_1 <- ksic8_ksic9 %>% filter(weight==1)
-ksic8_ksic9_2 <- ksic8_ksic9 %>% filter(weight!=1)
-
-
-
-check <- ksic8_ksic9_2 %>% distinct(ksic8) %>% pull(ksic8)
-
-
-est1996_1 <- est1996 %>% filter(!(ksic8 %in% check))
-
-est1996_2 <- est1996 %>% filter(ksic8 %in% check)
-
-est1996_1 <- est1996_1 %>%
-  left_join(ksic8_ksic9_1, by="ksic8") %>% select(-ksic8)
-
-
-
-ksic8_ksic9_2 <- ksic8_ksic9_2 %>%
-  left_join(est1996_2, by="ksic8") %>% select(-ksic8)
-
-
-est1996 <- est1996_1 %>%
-  bind_rows(ksic8_ksic9_2) %>% filter(!is.na(emp_all), !is.na(ksic9))
-
-
-
-#ksic 8 to 9 : 1999 only
-
-est1999 <- est1999 %>% group_by(iso, ksic8) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-ksic8_ksic9_1 <- ksic8_ksic9 %>% filter(weight==1)
-ksic8_ksic9_2 <- ksic8_ksic9 %>% filter(weight!=1)
-
-
-
-check <- ksic8_ksic9_2 %>% distinct(ksic8) %>% pull(ksic8)
-
-
-est1999_1 <- est1999 %>% filter(!(ksic8 %in% check))
-
-est1999_2 <- est1999 %>% filter(ksic8 %in% check)
-
-est1999_1 <- est1999_1 %>%
-  left_join(ksic8_ksic9_1, by="ksic8") %>% select(-ksic8)
-
-
-
-ksic8_ksic9_2 <- ksic8_ksic9_2 %>%
-  left_join(est1999_2, by="ksic8") %>% select(-ksic8)
-
-
-est1999 <- est1999_1 %>%
-  bind_rows(ksic8_ksic9_2) %>% filter(!is.na(emp_all), !is.na(ksic9))
-
-
-
-#ksic 8 to 9 : 2000 only
-
-est2000 <- est2000 %>% group_by(iso, ksic8) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-ksic8_ksic9_1 <- ksic8_ksic9 %>% filter(weight==1)
-ksic8_ksic9_2 <- ksic8_ksic9 %>% filter(weight!=1)
-
-
-
-check <- ksic8_ksic9_2 %>% distinct(ksic8) %>% pull(ksic8)
-
-
-est2000_1 <- est2000 %>% filter(!(ksic8 %in% check))
-
-est2000_2 <- est2000 %>% filter(ksic8 %in% check)
-
-est2000_1 <- est2000_1 %>%
-  left_join(ksic8_ksic9_1, by="ksic8") %>% select(-ksic8)
-
-
-
-ksic8_ksic9_2 <- ksic8_ksic9_2 %>%
-  left_join(est2000_2, by="ksic8") %>% select(-ksic8)
-
-
-est2000 <- est2000_1 %>%
-  bind_rows(ksic8_ksic9_2) %>% filter(!is.na(emp_all), !is.na(ksic9))
-
-
-
-#ksic 8 to 9 : 2001 only
-
-est2001 <- est2001 %>% group_by(iso, ksic8) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
-
-
-ksic8_ksic9_1 <- ksic8_ksic9 %>% filter(weight==1)
-ksic8_ksic9_2 <- ksic8_ksic9 %>% filter(weight!=1)
-
-
-
-check <- ksic8_ksic9_2 %>% distinct(ksic8) %>% pull(ksic8)
-
-
-est2001_1 <- est2001 %>% filter(!(ksic8 %in% check))
-
-est2001_2 <- est2001 %>% filter(ksic8 %in% check)
-
-est2001_1 <- est2001_1 %>%
-  left_join(ksic8_ksic9_1, by="ksic8") %>% select(-ksic8)
-
-
-
-ksic8_ksic9_2 <- ksic8_ksic9_2 %>%
-  left_join(est2001_2, by="ksic8") %>% select(-ksic8)
-
-
-est2001 <- est2001_1 %>%
-  bind_rows(ksic8_ksic9_2) %>% filter(!is.na(emp_all), !is.na(ksic9))
-
-
-
-# #ksic 9 to 10 : 1994, 1996, 1999, 2000, 2001 and 2010
-
-
-# est1994
-
-est1994 <- est1994 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1994 <- est1994 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-
-
-
-
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est1994_1 <- est1994 %>% filter(!(ksic9 %in% check))
-
-est1994_2 <- est1994 %>% filter(ksic9 %in% check)
-
-est1994_1 <- est1994_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est1994_2, by="ksic9") %>% select(-ksic9)
-
-
-est1994 <- est1994_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est1994 <- est1994 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1994 <- est1994 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-est1994 %>% haven::write_dta("data/final/hs_cpc_ksic/est1994_refine.dta")
-est1994 %>% write_rds("data/final/hs_cpc_ksic/est1994_refine.rds")
-
-
-# est1996
-
-est1996 <- est1996 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1996 <- est1996 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-
-
-
-
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est1996_1 <- est1996 %>% filter(!(ksic9 %in% check))
-
-est1996_2 <- est1996 %>% filter(ksic9 %in% check)
-
-est1996_1 <- est1996_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est1996_2, by="ksic9") %>% select(-ksic9)
-
-
-est1996 <- est1996_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est1996 <- est1996 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1996 <- est1996 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-est1996 %>% haven::write_dta("data/final/hs_cpc_ksic/est1996_refine.dta")
-est1996 %>% write_rds("data/final/hs_cpc_ksic/est1996_refine.rds")
-
-# est1999
-
-est1999 <- est1999 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1999 <- est1999 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-
-
-
-
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est1999_1 <- est1999 %>% filter(!(ksic9 %in% check))
-
-est1999_2 <- est1999 %>% filter(ksic9 %in% check)
-
-est1999_1 <- est1999_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est1999_2, by="ksic9") %>% select(-ksic9)
-
-
-est1999 <- est1999_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est1999 <- est1999 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est1999 <- est1999 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-est1999 %>% haven::write_dta("data/final/hs_cpc_ksic/est1999_refine.dta")
-est1999 %>% write_rds("data/final/hs_cpc_ksic/est1999_refine.rds")
-
-
-
-# est2000
-
-
-est2000 <- est2000 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est2000 <- est2000 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-
-
-
-
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est2000_1 <- est2000 %>% filter(!(ksic9 %in% check))
-
-est2000_2 <- est2000 %>% filter(ksic9 %in% check)
-
-est2000_1 <- est2000_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est2000_2, by="ksic9") %>% select(-ksic9)
-
-
-est2000 <- est2000_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est2000 <- est2000 %>% mutate(emp_all=emp_all*weight) %>% select(-weight)
-
-
-est2000 <- est2000 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T)) %>% ungroup()
-
-
-est2000 %>% haven::write_dta("data/final/hs_cpc_ksic/est2000_refine.dta")
-est2000 %>% write_rds("data/final/hs_cpc_ksic/est2000_refine.rds")
-
-
-# est2001
-
-est2001 <- est2001 %>% mutate(emp_all=emp_all*weight, emp_male=emp_male*weight, emp_female=emp_female*weight) %>% select(-weight)
-
-
-est2001 <- est2001 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
-
-
-
-
-
-
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est2001_1 <- est2001 %>% filter(!(ksic9 %in% check))
-
-est2001_2 <- est2001 %>% filter(ksic9 %in% check)
-
-est2001_1 <- est2001_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est2001_2, by="ksic9") %>% select(-ksic9)
-
-
-est2001 <- est2001_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est2001 <- est2001 %>% mutate(emp_all=emp_all*weight, emp_male=emp_male*weight, emp_female=emp_female*weight) %>% select(-weight)
-
-
-est2001 <- est2001 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
-
-
-est2001 %>% haven::write_dta("data/final/hs_cpc_ksic/est2001_refine.dta")
-est2001 %>% write_rds("data/final/hs_cpc_ksic/est2001_refine.rds")
-
-
-
-
-
-
-#est2010
-
-
-est2010 <- est2010 %>% group_by(iso, ksic9) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
-
-
-ksic9_ksic10_1 <- ksic9_ksic10 %>% filter(weight==1)
-ksic9_ksic10_2 <- ksic9_ksic10 %>% filter(weight!=1)
-
-
-
-check <- ksic9_ksic10_2 %>% distinct(ksic9) %>% pull(ksic9)
-
-
-est2010_1 <- est2010 %>% filter(!(ksic9 %in% check))
-
-est2010_2 <- est2010 %>% filter(ksic9 %in% check)
-
-est2010_1 <- est2010_1 %>%
-  left_join(ksic9_ksic10_1, by="ksic9") %>% select(-ksic9)
-
-
-
-ksic9_ksic10_2 <- ksic9_ksic10_2 %>%
-  left_join(est2010_2, by="ksic9") %>% select(-ksic9)
-
-
-est2010 <- est2010_1 %>%
-  bind_rows(ksic9_ksic10_2) %>% filter(!is.na(emp_all), !is.na(ksic10))
-
-
-est2010 <- est2010 %>% mutate(emp_all=emp_all*weight, emp_male=emp_male*weight, emp_female=emp_female*weight) %>% select(-weight)
-
-
-est2010 <- est2010 %>% group_by(iso, ksic10) %>% 
-  summarise(emp_all=sum(emp_all, na.rm=T), emp_female=sum(emp_female, na.rm=T), emp_male=sum(emp_male, na.rm=T)) %>% ungroup()
-
-
-est2010 %>% haven::write_dta("data/final/hs_cpc_ksic/est2010_refine.dta")
-est2010 %>% write_rds("data/final/hs_cpc_ksic/est2010_refine.rds")
-
-
-
-
+# download the finalized data
+processed_ksic10 %>% write_rds(here("data", "temp", "est_region_industry_matched.rds"))
